acf502e7b2db9960f73c2225aafefd98d1410077
Abdul Haseeb Ayub <ahaseeb.mail@gmail.com>
2018-10-04 22:23:02 +0200

    make arrays global static const

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -5,13 +5,13 @@
 
 using std::string;
 
+static const string ones[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+static const string tens[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+static const string hundreds[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+static const string thousands[] = {"", "M", "MM", "MMM"};
+
 string DecimalToRomanSimple(int decimalInput)
 {
-    string ones[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-    string tens[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-    string hundreds[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-    string thousands[] = {"", "M", "MM", "MMM"};
-
     string thousandth = thousands[decimalInput/1000];
     string hundredth = hundreds[(decimalInput%1000)/100];
     string tenth =  tens[(decimalInput%100)/10];
